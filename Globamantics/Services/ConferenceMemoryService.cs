ca94d739c683244664ab6442ffa3d47a838c448f
Desislava Stoyanova <Desislava.Stoyanova@snowsoftware.com>
2018-12-05 13:48:14 +0100

    Globamantics - exercise project to understand ASP:NET Core

---
+++ Globamantics/Services/ConferenceMemoryService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Globamantics.Services
+{
+    public class ConferenceMemoryService : IConferenceService
+    {
+        public List<ConferenceModel> conferences = new List<ConferenceModel>();
+        
+        public ConferenceMemoryService()
+        {
+            conferences.Add(new ConferenceModel { Id = 1, Name = "NDC", Location = "Oslo", Start = DateTime.Now, AttendeeTotal = 200});
+            conferences.Add(new ConferenceModel { Id = 2, Name = "IT/DevConnections", Location = "Stockholm", Start = DateTime.Now, AttendeeTotal = 100});
+        }
+
+        public Task Add(ConferenceModel model)
+        {
+            model.Id = conferences.Max(c => c.Id) + 1;
+            conferences.Add(model);
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<ConferenceModel>> GetAll()
+        {
+            return Task.Run(() => conferences.AsEnumerable());
+        }
+
+        public Task<ConferenceModel> GetById(int id)
+        {
+            return Task.Run(() => conferences.FirstOrDefault(c => c.Id == id));
+        }
+
+        public Task<StatisticsModel> GetStatistics()
+        {
+            return Task.Run(() =>
+            {
+                return new StatisticsModel
+                {
+                    NumberOfAttendees = conferences.Sum(c => c.AttendeeTotal),
+                    AverageConferenceAttendees = (int)conferences.Sum(c => c.AttendeeTotal) / 2
+                };
+            });
+        }
+    }
+}
---
+++ Globamantics/Services/ProposalMemoryService.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Globamantics.Services
+{
+    public class ProposalMemoryService : IProposalService
+    {
+        public List<ProposalModel> proposals = new List<ProposalModel>();
+
+        public ProposalMemoryService()
+        {
+            proposals.Add(new ProposalModel
+            {
+                Id = 1,
+                ConferenceId = 1,
+                Speaker = "Roland Guijt",
+                Title = "Understanding .NET Core Security",
+                Approved = false
+            });
+            proposals.Add(new ProposalModel
+            {
+                Id = 2,
+                ConferenceId = 1,
+                Speaker = "Roland Guijt",
+                Title = "Understanding .NET Core 2.x",
+                Approved = false
+            });
+            proposals.Add(new ProposalModel
+            {
+                Id = 3,
+                ConferenceId = 2,
+                Speaker = "Karam Malkon",
+                Title = "Understanding React",
+                Approved = false
+            });
+        }
+
+        public Task Add(ProposalModel model)
+        {
+            model.Id = proposals.Max(p => p.Id) + 1;
+            proposals.Add(model);
+            return Task.CompletedTask;
+        }
+
+        public Task<ProposalModel> Approve(int proposalId)
+        {
+            return Task.Run(() =>
+            {
+                var proposal = proposals.FirstOrDefault(p => p.Id == proposalId);
+                proposal.Approved = true;
+                return proposal;
+            });
+        }
+
+        public Task<IEnumerable<ProposalModel>> GetAll(int conferenceId)
+        {
+            return Task.Run(() => proposals.Where(p => p.ConferenceId == conferenceId).AsEnumerable());
+        }
+    }
+}
